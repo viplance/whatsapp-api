7ab9b89735ce127f9ad1d8356071ead6099ce605
Dzmitry Sharko <viplance.com@gmail.com>
2025-06-22 17:40:46 +0300

    Added Swagger module.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
@@ -6,6 +7,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get('SERVER_PORT');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('WhatsApp direct message API')
+    .setDescription('Send direct messages to WhatsApp without Business account')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, documentFactory);
 
   await app.listen(port || 3000);
 }
