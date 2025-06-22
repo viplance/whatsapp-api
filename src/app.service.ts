3743d57ea88c73fdfe52bd1924dccbb92559d21e
Dzmitry Sharko <viplance.com@gmail.com>
2025-06-22 17:36:13 +0300

    Added ConfigService

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'WhatsApp direct message API';
+  }
+
+  sendMessage() {
+    return 'Message sent';
   }
 }
