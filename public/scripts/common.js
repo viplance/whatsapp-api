d55b080a33ec3740dd0753e3d762bfb74ee59ccb
Dzmitry Sharko <viplance.com@gmail.com>
2025-07-27 16:01:39 +0300

    Design has been improved.

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -3,4 +3,15 @@ window.addEventListener('load', () => {
   document
     .getElementById('phone')
     .setAttribute('href', 'https://wa.me/1111111111');
+
+  // smooth anchor scroll
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth',
+      });
+    });
+  });
 });
